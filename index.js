2d675936b695a3e9f117a54451dc240889b49590
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2022-12-10 20:19:06 +0300

    Arrays complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(name){
+    const AppendCat = cats.push("Ralph");
+
+  }
+  function destructivelyPrependCat(name){
+    const prependCat = cats.unshift("Bob");
+  }
+
+  function destructivelyRemoveLastCat(name){
+    const removeLast = cats.splice(2);
+  }
+  function destructivelyRemoveFirstCat(name){
+    const removeFirst = cats.splice(0,1);
+  }
+
+  
+  function appendCat(name){
+    return [...cats, name];
+  }
+  function prependCat(name){
+    return [name, ...cats];
+  }
+  function removeFirstCat(){
+    return cats.slice(1);
+  }
+  function removeLastCat(){
+    return cats.slice(0, cats.length -1);
+
+  }
